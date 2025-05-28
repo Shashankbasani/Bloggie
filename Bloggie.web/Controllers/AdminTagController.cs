2a612beb5cbac9980f3c91f9a2f4c75101626e33
Shashankbasani <shashankbasani44@gmail.com>
2025-05-28 20:29:27 +0530

    adding list of tags

--- Bloggie.web/Controllers/AdminTagController.cs
+++ Bloggie.web/Controllers/AdminTagController.cs
@@ -1,13 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggie.web.Data;
+using Bloggie.web.Models.Domain;
+using Bloggie.web.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.web.Controllers
 {
     public class AdminTagController : Controller
     {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public AdminTagController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            //var name = Request.Form["name"];
+            //var displayName = Request.Form["displayName"];
+
+            // mapping AddTagRequest to domain model
+            var tag = new Tag()
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+
+            bloggieDbContext.Tags.Add(tag);
+
+            bloggieDbContext.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            var tags = bloggieDbContext.Tags.ToList();
+
+            return View(tags);
         }
     }
 }
